8e8a18ebeb35a66c866cc9848c02401403c93c3d
Lucas Boz <lucas_boz@hotmail.com>
2017-06-30 02:27:23 -0300

    Ajustes em listagem e visualização de detalhes

--- app/javascript/home/controllers/search-controller.js
+++ app/javascript/home/controllers/search-controller.js
@@ -2,6 +2,7 @@ const mod = require('../module');
 require('./search-controller.css');
 module.exports = function SearchController($scope, $mdSidenav) {
 
+    $scope.attraction = {}
     $scope.roteiro =
         [ // dias
             {
@@ -69,32 +70,18 @@ module.exports = function SearchController($scope, $mdSidenav) {
         // $mdSidenav('menuSideNav').toggle();
     };
 
-    $scope.toggleLeft = buildToggler('left');
-    $scope.toggleRight = buildToggler('right');
 
-    function buildToggler(componentId) {
-        return function () {
-            $mdSidenav(componentId).toggle();
-        };
+    $scope.openDetailsSidenav = function (attraction) {
+        $scope.attraction = attraction;
+        $mdSidenav('right').toggle();
+    }
+    $scope.closeDetailsSidenav = function () {
+        $mdSidenav('right').toggle();
     }
 
-    $scope.attraction = {
-        id: 1,
-        name: "Vista Panorâmica ",
-        duration: "02:00",
-        start_time: "10:00",
-        description: "A Usina Hidrelétrica de Itaipu é um dos projetos de engenharia mais ambiciosos do mundo. Uma usina que fornece 16,4% da energia consumida no Brasil e abastece 71,3% do consumo paraguaio. Construída para reter o curso do Rio Paraná, a barragem é uma imensa estrutura de concreto de oito quilômetros de comprimento por 167 metros de altura, que contorna o reservatório da Itaipu Binacional e sobre a qual estão instalados os dutos e as unidades geradoras de energia elétrica.",
-        observations: "Não é permitido entrar com bolsas e mochilas no Circuito Especial. Há armários no Centro de Recepção de Visitantes para alugar.",
-        adult_price: 36.00,
-        adults_only: false,
-        picture: "https://www.turismoitaipu.com.br/sites/default/files/atracoes/foto_grande.jpg",
-        place: {
-            id: 1,
-            name: "ITAIPU BINACIONAL",
-            picture: "http://www.loumarturismo.com.br/passeios/imagens/00008/1066-1-copy.jpg",
-            phone: "(45) 98805-9556",
-            website: "https://www.turismoitaipu.com.br/"
-        }
-    }
+
+
+
+
 
 };
